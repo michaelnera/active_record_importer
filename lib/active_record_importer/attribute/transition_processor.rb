86f1941165cbb6d1ebf49acc1f6c1969f854f823
Michael T. Nera <michael.nera@stellarloyalty.com>
2017-02-20 20:09:32 +0800

    instance transition processor

---
+++ lib/active_record_importer/attribute/transition_processor.rb
@@ -0,0 +1,39 @@
+module ActiveRecordImporter
+  class TransitionProcessor
+
+    attr_reader :object, :column, :new_state
+
+    def initialize(object, new_state, column = :state)
+      @object = object
+      @new_state = new_state
+      @column = column
+    end
+
+    def transit
+      fire_event!(transit_event)
+      true
+    end
+
+    private
+
+    def fire_event!(event)
+      fail 'Transition is invalid.' if event.blank?
+      object.send(event)
+    end
+
+    def transit_event
+      state_transitions.each do |trans|
+        return trans.event if trans.from == state_was && trans.to == new_state
+      end
+      nil
+    end
+
+    def state_transitions
+      object.send("#{column}_transitions")
+    end
+
+    def state_was
+      object.send("#{column}_was")
+    end
+  end
+end
