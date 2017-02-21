847b99425f26eed5a1f18ebc6fe41253bab5c12f
Michael T. Nera <michael.nera@stellarloyalty.com>
2017-02-21 19:05:57 +0800

    add and remove import options

--- lib/active_record_importer/options_builder.rb
+++ lib/active_record_importer/options_builder.rb
@@ -5,32 +5,41 @@ module ActiveRecordImporter
     end
   end
 
+  #
+  # SmarterCSV is used to process the csv file
+  # https://github.com/tilo/smarter_csv
+  # I used some of it's options
+  # Please refer to it's documentation
+  #
   class CsvOptions
     include Virtus.value_object
 
     values do
-      attribute :convert_values_to_numeric, Hash[Symbol => Array],
-                default: { only: [:metric_amount] }
+      attribute :convert_values_to_numeric
+      attribute :value_converters, Hash, default: nil
       attribute :remove_empty_values, Boolean, default: false
       attribute :comment_regexp, Regexp, default: Regexp.new(/^#=>/)
       attribute :force_utf8, Boolean, default: true
+      attribute :chunk_size, Integer, default: 500
+      attribute :col_sep, String, default: ','
     end
   end
 
+  #
+  # Import Option for every IMPORTABLE model
+  #
   class ImporterOptions
     include Virtus.model
 
-    attribute :required_attributes, Array
     attribute :find_options, Array
     attribute :exclude_from_find_options, Array
-    attribute :import_method, Symbol, default: :create
     attribute :scope, Symbol
     attribute :importable_columns, Array
     attribute :default_attributes, Hash
     attribute :csv_opts, CsvOptions, default: CsvOptions.new
     attribute :find_assoc_opts, Hash
-    attribute :before_assign_of_attrs, Proc
-    attribute :after_save_callbacks, Array
+    attribute :before_save
+    attribute :after_save
     attribute :state_machine_attr, Array
   end
 end
