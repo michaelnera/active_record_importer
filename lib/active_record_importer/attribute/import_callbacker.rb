32a02bf1baeb6e4faf8933541cadb31aa1fb5d21
Michael T. Nera <michael.nera@stellarloyalty.com>
2017-02-21 19:24:09 +0800

    allow array, proc and methods for after_save callbacks

--- lib/active_record_importer/attribute/import_callbacker.rb
+++ lib/active_record_importer/attribute/import_callbacker.rb
@@ -8,6 +8,19 @@ module ActiveRecordImporter
     end
 
     def call
+      case callback_methods
+        when Array
+          run_each_callbacks
+        when Symbol
+          object.send(callback)
+        when Proc
+          callback.call(object)
+      end
+    end
+
+    private
+
+    def run_each_callbacks
       callback_methods.each do |callback|
         object.send(callback) if callback.is_a?(Symbol)
         callback.call(object) if callback.is_a?(Proc)
